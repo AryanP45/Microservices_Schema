c749dd7796af61033cdc34cdec44281efc6bae2d
Aryan Patil <aryanitinpatil@gmail.com>
2024-04-10 23:14:16 +0530

    Create OrderLineItemsDto.java

---
+++ Order-service/src/main/java/com/aryanp45/orderservice/dto/OrderLineItemsDto.java
@@ -0,0 +1,23 @@
+package com.aryanp45.orderservice.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItemsDto {
+	
+	private Long id;
+	
+	private String skuCode;
+	
+	private BigDecimal price;
+	
+	private Integer quantity;
+
+}
