4f6bef3d4d2579de07e1328f021c2b7f0c9eca54
Aryan Patil <aryanitinpatil@gmail.com>
2024-04-10 23:18:33 +0530

    added missing annotation in inventory repo

--- inventory-service/src/main/java/com/aryanp45/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/aryanp45/inventoryservice/repository/InventoryRepository.java
@@ -1,13 +1,13 @@
 package com.aryanp45.inventoryservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.aryanp45.inventoryservice.model.Inventory;
 
 import java.util.List;
-import java.util.Optional;
 
-
+@Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long>{
 	
 	List<Inventory> findBySkuCodeIn(List<String> skuCode);
