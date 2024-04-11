c152f9b2ac92ecb518fc9b8e2d0b68ab95d06212
Aryan Patil <aryanitinpatil@gmail.com>
2024-04-11 23:06:02 +0530

    security config in discovery server
    disabled csrf for /eureka

---
+++ Discovery-server/src/main/java/com/aryanp45/discoveryservice/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.aryanp45.discoveryservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf(csrf -> csrf.ignoringRequestMatchers("/eureka/**"));
+		return httpSecurity.build();
+
+	}
+}
